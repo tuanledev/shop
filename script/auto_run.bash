78ad979551567fb37beb1d4641c4c32b9eabb717
tuanlt <tuanlt21@fpt.com.vn>
2018-12-07 15:34:18 +0700

    .

--- script/auto_run.bash
+++ script/auto_run.bash
@@ -2,5 +2,5 @@ if lsof -Pi :9090 -sTCP:LISTEN -t >/dev/null ;
 then
     echo "running"
 else
-    nohup bee run /home/project/src/shop &
+    nohup /home/project/src/shop/shop >/dev/null 2>&1 &
 fi
